02fce8da16598df6beb6794daa4fd8047dad7ebd
ndrooo <git@ndr.ooo>
2023-01-28 10:39:35 -0500

    create an initial config file for kiwi

---
+++ nixos/kiwi/configuration.nix
@@ -0,0 +1,55 @@
+{ inputs, lib, config, pkgs, ... }: {
+  # You can import other NixOS modules here
+  imports = [
+    # If you want to use modules from other flakes (such as nixos-hardware):
+    # inputs.hardware.nixosModules.common-cpu-amd
+    # inputs.hardware.nixosModules.common-ssd
+
+    # You can also split up your configuration and import pieces of it here:
+    # ./users.nix
+
+    # Import your generated (nixos-generate-config) hardware configuration
+    ../base.nix
+    ../graphics.nix
+    # ./hardware-configuration.nix
+  ];
+
+  networking.hostName = "kiwi";
+
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = false;
+  /*
+  boot.initrd.luks.devices = {
+    nix = {
+      device = "/dev/disk/by-uuid/###";
+      preLVM = true;
+      allowDiscards = true;
+    };
+  };
+  */
+
+  /*
+  environment.etc.crypttab = {
+    enable = true;
+    text = ''
+      selectron UUID=### /root/kiwi.key luks,nofail
+    '';
+  };
+
+  environment.etc.fstab = {
+    enable = true;
+    text = ''
+      UUID=### /selectron ext4 rw,relatime,nofail 0 2
+    '';
+  };
+  */
+
+  services.xserver.videoDrivers = [ "nvidia" ];
+  hardware.opengl.enable = true;
+
+  users.users.ndrooo = {
+    isNormalUser = true;
+    extraGroups = [ "wheel" ];
+    shell = pkgs.fish;
+  };
+}
