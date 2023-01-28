1e956e4d71e6eeacd1d3fe35a5f9003357cb4516
ndrooo <git@ndr.ooo>
2023-01-28 01:16:26 -0500

    fish + starship setup

--- home/lyoko.nix
+++ home/lyoko.nix
@@ -54,6 +54,45 @@
     envFile.text = ''
     '';
   };
+  
+  programs.fish = {
+    enable = true;
+    interactiveShellInit = ''
+      set fish_greeting # no more greeting
+      fish_vi_key_bindings
+    '';
+    plugins = [
+      { name = "pisces"; src = pkgs.fishPlugins.pisces.src; }
+    ];
+  };
+
+  programs.starship = {
+    enable = true;
+    enableNushellIntegration = true;
+    enableFishIntegration = true;
+    settings = {
+      character = {
+        success_symbol = "💖";
+        error_symbol = "💀";
+        vimcmd_symbol = "🔮";
+        vimcmd_replace_one_symbol = "🔂";
+        vimcmd_replace_symbol = "🔁";
+        vimcmd_visual_symbol = "📋";
+      };
+      status = {
+        disabled = false;
+        map_symbol = true;
+        symbol = "";
+        not_executable_symbol = "🚫 [NEX](bold red)";
+        not_found_symbol = "🔍 [NFD](bold red)";
+        sigint_symbol = "✋ [INT](bold yellow)";
+        signal_symbol = "🏁 [SIG](bold yellow)";
+        format = "$symbol ";
+      };
+      git_branch.symbol = "🔀 ";
+      sudo.disabled = false;
+    };
+  };
 
   # Nicely reload system units when changing configs
   systemd.user.startServices = "sd-switch";
