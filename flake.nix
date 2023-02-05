bec6e824a94ab56ebd0eb56e202c7bc359168f74
ndrooo <git@ndr.ooo>
2023-02-05 17:23:42 -0500

    cleanup: unneeded comments

--- flake.nix
+++ flake.nix
@@ -2,21 +2,13 @@
   description = "ndrooo nix config";
 
   inputs = {
-    # Nixpkgs
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-
-    # Home manager
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
-
-    # Shameless plug: looking for a way to nixify your themes and make
-    # everything match nicely? Try nix-colors!
-    # nix-colors.url = "github:misterio77/nix-colors";
   };
 
   outputs = { nixpkgs, home-manager, ... }@inputs: {
-    # NixOS configuration entrypoint
-    # Available through 'nixos-rebuild --flake .#your-hostname'
+    # nixos-rebuild --flake .#your-hostname
     nixosConfigurations.lyoko = nixpkgs.lib.nixosSystem {
       specialArgs = { inherit inputs; };
       modules = [
@@ -30,16 +22,15 @@
       ];
     };
 
-    # Standalone home-manager configuration entrypoint
-    # Available through 'home-manager --flake .#your-username@your-hostname'
+    # home-manager --flake .#your-username@your-hostname
     homeConfigurations.lyoko = home-manager.lib.homeManagerConfiguration {
-      pkgs = nixpkgs.legacyPackages.x86_64-linux; # Home-manager requires 'pkgs' instance
-      extraSpecialArgs = { inherit inputs; }; # Pass flake inputs to our config
+      pkgs = nixpkgs.legacyPackages.x86_64-linux;
+      extraSpecialArgs = { inherit inputs; };
       modules = [ ./home/lyoko.nix ];
     };
     homeConfigurations.kiwi = home-manager.lib.homeManagerConfiguration {
-      pkgs = nixpkgs.legacyPackages.x86_64-linux; # Home-manager requires 'pkgs' instance
-      extraSpecialArgs = { inherit inputs; }; # Pass flake inputs to our config
+      pkgs = nixpkgs.legacyPackages.x86_64-linux;
+      extraSpecialArgs = { inherit inputs; };
       modules = [ ./home/kiwi.nix ];
     };
   };
