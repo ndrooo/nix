f95a42b744bfbf65014e0e04ffbce60aa251ba15
ndrooo <git@ndr.ooo>
2023-02-05 23:12:14 -0500

    🧹 accidentally created extra nix files

--- home/polybar/polybar.nix
+++
@@ -1,7 +0,0 @@
-{
-  xdg.configFile = {
-    "polybar/bars.ini".source = ./polybar/bars.ini;
-    "polybar/modules.ini".source = ./polybar/modules.ini;
-    "polybar/user_modules.ini".source = ./polybar/user_modules.ini;
-  };
-}
--- home/rofi/rofi.nix
+++
@@ -1,6 +0,0 @@
-{
-  xdg.configFile = {
-    "rofi/config.rasi".source = ./rofi/config.rasi;
-    "rofi/theme.rasi".source = ./rofi/theme.rasi;
-  };
-}
