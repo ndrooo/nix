a20d4f883f022b0238ad0752aa0e53063568d220
ndrooo <git@ndr.ooo>
2023-02-24 00:44:26 -0500

    alacritty: basic config, unused

---
+++ home/modules/alacritty.nix
@@ -0,0 +1,10 @@
+{
+  programs.alacritty.enable = true;
+  programs.alacritty.settings = {
+    font = {
+      normal.family = "VictorMono Nerd Font";
+    };
+    window.padding = { x = 10; y = 10; };
+    window.dynamic_padding = true;
+  };
+}
