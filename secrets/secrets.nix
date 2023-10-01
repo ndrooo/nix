990de5245d8077a1dbb4aa81ce4bcaba5244ce8b
ndrooo <git@ndr.ooo>
2023-10-01 18:42:03 -0400

    add xana public key

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,7 +1,7 @@
 let
   lyoko = "";
   kiwi = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDXpG+hRrhYEWhrrtGpQw7VnGc+TnCFY4Nl2HP1uSHFk root@kiwi";
-  xana = "";
+  xana = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIM6aqQ2bH4RWya6z2/4sZ5rqlhGRzIIP++moIZ579J/5 root@xana";
   hosts = [ lyoko kiwi xana ];
 in
 {
