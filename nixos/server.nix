93ff24ede3d82f5e684ed8b0fbbfa4529e458251
ndrooo <git@ndr.ooo>
2024-08-07 15:44:39 -0400

    wireguard on xana

--- nixos/server.nix
+++ nixos/server.nix
@@ -1,6 +1,23 @@
 { pkgs, ... }: {
   imports = [];
 
+  networking.wg-quick.interfaces = {
+    wg0 = {
+      address = [ "10.0.0.2/24" "fdc9:281f:04d7:9ee9::2/64" ];
+      dns = [ "10.0.0.1" "fdc9:281f:04d7:9ee9::1" ];
+      privateKeyFile = "/root/wireguard-keys/privatekey";
+      
+      peers = [
+        {
+          publicKey = "KwU9qcRO0eamCxh/iF7pL2RAOOJezHPVqkIeANcS5Wk=";
+          allowedIPs = [ "0.0.0.0/0" "::/0" ];
+          endpoint = "154.47.25.129:51820";
+          persistentKeepalive = 25;
+        }
+      ];
+    };
+  };
+
   virtualisation.oci-containers = {
     backend = "podman";
     containers.homarr = {
