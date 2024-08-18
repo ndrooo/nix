221eecfdcf8f59954f5f3150dcc77b7ea0f62966
ndrooo <git@ndr.ooo>
2024-08-18 16:56:22 -0400

    xana: +gluetun/qbittorrent (not working yet)

--- nixos/server.nix
+++ nixos/server.nix
@@ -20,6 +20,26 @@
 
   virtualisation.oci-containers = {
     backend = "podman";
+    containers.gluetun = {
+      image = "qmcgaw/gluetun";
+      ports = [
+        "6081:6881"
+        "6081:6881/udp"
+        "6011:6011"
+      ];
+      volumes = [
+        "/root/gluetun:/gluetun"
+      ];
+      environment = {
+        VPN_SERVICE_PROVIDER = "protonvpn";
+        VPN_TYPE = "wireguard";
+        SERVER_COUNTRIES="United States";
+        PORT_FORWARD_ONLY="on";
+        VPN_PORT_FORWARDING="on";
+      };
+      environmentFiles = [/root/wireguard-keys/wg.env];
+      extraOptions = ["--cap-add=NET_ADMIN" "--device=/dev/net/tun"];
+    };
     containers.homarr = {
       image = "ghcr.io/ajnart/homarr:latest";
       ports = ["7575:7575"];
@@ -31,6 +51,21 @@
       extraOptions = [ 
         "--network=host" 
       ];
+    };
+    containers.qbittorrent = {
+      image = "ghcr.io/linuxserver/qbittorrent";
+      environment = {
+        PUID = "1000";
+        PGID = "100";
+        TZ = "America/New_York";
+        WEBUI_PORT = "6011";
+      };
+      dependsOn = [ "gluetun" ];
+      volumes = [
+        "/twistor:/twistor"
+        "/root/qbittorrent/config:/config"
+      ];
+      extraOptions = ["--network=container:gluetun"];
     };
   };
   networking.firewall.allowedTCPPorts = [ 8123 ]; # for home-assistant
