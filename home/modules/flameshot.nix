094e7979deb29a5a2c422ac3b72fe61eab548e10
ndrooo <git@ndr.ooo>
2024-12-24 22:50:54 -0500

    flameshot: wayland support

--- home/modules/flameshot.nix
+++ home/modules/flameshot.nix
@@ -1,5 +1,6 @@
-{
+{ pkgs, ... }: {
   services.flameshot.enable = true;
+  services.flameshot.package = (pkgs.flameshot.override { enableWlrSupport = true; });
   services.flameshot.settings = {
     General = {
       disabledTrayIcon = true;
