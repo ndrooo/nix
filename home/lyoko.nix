fb4b1b94efe144e1fb759f46f6b0772f36593415
ndrooo <git@ndr.ooo>
2026-07-12 15:09:23 -0400

    Add home manager graphics back to lyoko

--- home/lyoko.nix
+++ home/lyoko.nix
@@ -1,9 +1,16 @@
-{ ... }: 
+{ ... }:
 let
   modules = import ./modules;
-in {
+in
+{
   imports = [
     ./base.nix
     modules.cli
+    modules.graphical
+    modules.sway
+    modules.kitty
+    ./themes/sumac.nix
   ];
+
+  services.syncthing.enable = true;
 }
