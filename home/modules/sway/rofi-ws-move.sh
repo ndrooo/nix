e9ba2fbc0d94916df7157f9cce33092edc308c7a
ndrooo <git@ndr.ooo>
2026-07-15 09:11:30 -0400

    Strip quotes from rofi workspace script

--- home/modules/sway/rofi-ws-move.sh
+++ home/modules/sway/rofi-ws-move.sh
@@ -2,7 +2,7 @@
 
 if [ x"$1" = x ]
 then
-	swaymsg -t get_workspaces | jq '.[] | .name'
+	swaymsg -t get_workspaces | jq '.[] | .name' | tr -d '"'
 else
 	swaymsg "move container to workspace $1" >/dev/null
 fi
